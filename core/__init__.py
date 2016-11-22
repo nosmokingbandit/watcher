b46560e12d65a681188beb410d602f78872ec852
nosmokingbandit <nosmokingbandit@gmail.com>
2016-11-22 12:22:47 -0500

    add core vars in init

---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+# Paths to various things
+PROG_PATH = ''
+GIT_URL = 'https://github.com/nosmokingbandit/watcher'
+GIT_REPO = 'https://github.com/nosmokingbandit/watcher.git'
+GIT_API = 'https://api.github.com/repos/nosmokingbandit/watcher'
+GIT_BRANCH = 'testing'
+
+# Update info
+UPDATE_STATUS = None
+UPDATE_LAST_CHECKED = None
+UPDATING = False

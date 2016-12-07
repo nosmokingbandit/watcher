dcd1e5b14503c0e3b0d8b57fe7b71f39f631e71e
nosmokingbandit <nosmokingbandit@gmail.com>
2016-12-07 17:05:38 -0500

    Remove post-processing from API application

--- core/api.py
+++ core/api.py
@@ -3,6 +3,7 @@ import cherrypy
 import urllib2
 from urlparse import parse_qs
 import config
+import core
 from core import config, sqldb, snatcher, postprocessing
 
 import logging
@@ -10,7 +11,7 @@ logging = logging.getLogger(__name__)
 
 class API(object):
     '''
-    A simple GET/POST api. Used for post-processing and other remote interactions.
+    A simple GET/POST api. Used for basic remote interactions.
     This still needs work.
     '''
     conf = {
@@ -22,49 +23,39 @@ class API(object):
 
     def __init__(self):
         self.sql = sqldb.SQL()
-        self.post = postprocessing.PostProcessing()
 
-
-    def GET(self, params):
+    def GET(self, **params):
         serverkey = core.CONFIG['Server']['apikey']
 
-        params = parse_qs(params)
-        # parse_qs parses as {key:[val]}, so we 'de-list' the values
-        for k, v in params.iteritems():
-            params[k] = v[0]
 
         # then we need to reconstruct the guid
         if 'i' in params and 'r' in params:
             params['guid'] = '{}&i={}&r={}'.format(params['guid'], params['i'], params['r'])
 
+        # check for api key
         if serverkey != params['apikey']:
             logging.warning('Invalid API key in request.')
             return 'Incorrect API Key.'
 
+        # find what we are going to do
         if 'mode' not in params:
             return 'No API mode specified.'
+
+
         if params['mode'] == 'liststatus':
-            logging.info('API request movie list.')
-            movies = self.sql.get_user_movies()
-            if not movies:
-                return False
-            l = []
+            return self.liststatus()
+
+
+    def liststatus(self):
+        logging.info('API request movie list.')
+        l = []
+        movies = self.sql.get_user_movies()
+        if not movies:
+            return l
+        else:
             for movie in movies:
                 l.append(dict(movie))
             return json.dumps(l, indent=1)
-
-        if 'guid' not in params:
-            return 'No GUID Supplied. Something went wrong with the postprocessing script.'
-        if 'path' not in params:
-            return 'No path Supplied. Something went wrong with the postprocessing script.'
-
-        guid = urllib2.unquote(params['guid'])
-        path = urllib2.unquote(params['path'])
-        if params['mode'] == 'failed':
-            return self.post.failed(guid, path)
-        if params['mode'] == 'complete':
-            return self.post.complete(guid, path)
-
 
 
 

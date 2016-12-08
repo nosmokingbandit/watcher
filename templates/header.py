4aae88001f495e4f793ddaf8e6e7642602606ab7
nosmokingbandit <nosmokingbandit@gmail.com>
2016-12-08 15:10:05 -0500

    Center header content

--- templates/header.py
+++ templates/header.py
@@ -7,12 +7,13 @@ class Header():
         menu_items = ['Settings', 'Add Movie', 'Status']
 
         with div(id='header'):
-            img(src='images/logo.png', alt='')
-            with ul(id='nav'):
-                for i in menu_items:
-                    i_lower = i.lower().replace(' ','_')
-                    with a(href='/{}'.format(i_lower)):
-                        if i_lower == current:
-                            li( i, cls=(i_lower + ' current') )
-                        else:
-                            li( i, cls=i_lower )
+            with div(id='header_container'):
+                img(src='images/logo.png', alt='')
+                with ul(id='nav'):
+                    for i in menu_items:
+                        i_lower = i.lower().replace(' ','_')
+                        with a(href='/{}'.format(i_lower)):
+                            if i_lower == current:
+                                li( i, cls=(i_lower + ' current') )
+                            else:
+                                li( i, cls=i_lower )

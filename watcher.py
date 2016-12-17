6a96eb5aafcca99172739c5cfc55fda541b083e2
nosmokingbandit <nosmokingbandit@gmail.com>
2016-12-17 11:36:20 -0500

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -144,6 +144,9 @@ class Scheduler(object):
 
         @staticmethod
         def create():
+            print '================'
+            print ' THIS IS A TEST '
+            print '================'
             ver = version.Version()
             interval = int(core.CONFIG['Server']['checkupdatefrequency']) * 3600
 

e3d38c57af49465b29c560a3a4f3b0af35211043
soundjester <marcus.burke@tamuc.edu>
2016-12-24 13:14:13 -0600

    Update update.py

--- templates/update.py
+++ templates/update.py
@@ -1,6 +1,7 @@
 import cherrypy
 import core
 import dominate
+from core import config
 from cherrypy import expose
 from dominate.tags import *
 
@@ -13,13 +14,15 @@ class Update():
             raise cherrypy.HTTPRedirect("/status")
             return
 
+	theme=str(core.CONFIG['Server']['csstheme'])
+		
         doc = dominate.document(title='Watcher')
 
         with doc.head:
             base(href="/static/")
 
-            link(rel='stylesheet', href='css/style.css')
-            link(rel='stylesheet', href='css/update.css')
+            link(rel='stylesheet', href='css/'+theme+'/style.css')
+            link(rel='stylesheet', href='css/'+theme+'/update.css')
             link(rel='stylesheet', href='//fonts.googleapis.com/css?family=Raleway')
 
             script(type='text/javascript', src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js')

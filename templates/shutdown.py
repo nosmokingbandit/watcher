358c64de53a5c8256783a5beadf01693f568094b
soundjester <marcus.burke@tamuc.edu>
2016-12-24 13:13:19 -0600

    Update shutdown.py

--- templates/shutdown.py
+++ templates/shutdown.py
@@ -1,4 +1,6 @@
 import dominate
+import core
+from core import config
 from cherrypy import expose
 from dominate.tags import *
 
@@ -7,13 +9,15 @@ class Shutdown():
 
     @expose
     def index(self):
+	theme=str(core.CONFIG['Server']['csstheme'])
+		
         doc = dominate.document(title='Watcher')
 
         with doc.head:
             base(href="/static/")
 
-            link(rel='stylesheet', href='css/style.css')
-            link(rel='stylesheet', href='css/shutdown.css')
+            link(rel='stylesheet', href='css/'+theme+'/style.css')
+            link(rel='stylesheet', href='css/'+theme+'/shutdown.css')
             link(rel='stylesheet', href='//fonts.googleapis.com/css?family=Raleway')
 
             script(type='text/javascript', src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js')

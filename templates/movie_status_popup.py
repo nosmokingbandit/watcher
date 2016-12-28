0bedc7d324aa7f683c2e8e164a1b9461538903ba
nosmokingbandit <nosmokingbandit@gmail.com>
2016-12-28 14:13:21 -0500

    Better sub-page rendering

--- templates/movie_status_popup.py
+++ templates/movie_status_popup.py
@@ -23,79 +23,75 @@ class MovieStatusPopup():
 
         quality_settings = json.loads(data['quality'])
 
-        doc = dominate.document(title='Watcher')
+        container = div(id='container')
+        with container:
+            script(src=core.URL_BASE + '/static/js/status/movie_status_popup.js?v=12.28')
+            if not data:
+                span('Unable to get movie information from database. Check logs for more information.')
+                return doc.render()
 
-        with doc.head:
-            script(src=core.URL_BASE + '/static/js/status/movie_status_popup.js?v=12.27')
+            with div(id='title'):
+                with p():
+                    span(title_date, id='title', imdbid=imdbid)
+                    i(cls='fa fa-times', id='close')
+                    i(cls='fa fa-trash', id='remove')
+                    i(cls='fa fa-cog', id='change_quality')
+                    i(cls='fa fa-save', id='save_quality')
+                    i(cls='fa fa-search', id='search_now', imdbid=data['imdbid'], title=data['title'])
+            with div(id='media'):
+                img(id='poster', src=poster_path)
+                with div(id='search_results'):
 
-        with doc:
-            with div(id='container'):
-                if not data:
-                    span('Unable to get movie information from database. Check logs for more information.')
-                    return doc.render()
+                    with ul(id='result_list'):
+                        self.result_list(imdbid)
+                    div(id='results_thinker')
 
-                with div(id='title'):
-                    with p():
-                        span(title_date, id='title', imdbid=imdbid)
-                        i(cls='fa fa-times', id='close')
-                        i(cls='fa fa-trash', id='remove')
-                        i(cls='fa fa-cog', id='change_quality')
-                        i(cls='fa fa-save', id='save_quality')
-                        i(cls='fa fa-search', id='search_now', imdbid=data['imdbid'], title=data['title'])
-                with div(id='media'):
-                    img(id='poster', src=poster_path)
-                    with div(id='search_results'):
+                    # Panel that swaps in with quality adjustments
+                    resolutions = ['4K', '1080P', '720P', 'SD']
+                    with ul(id='quality', cls='wide'):
+                        # Resolution Block
+                        with ul(id='resolution', cls='sortable'):
+                            span('Resolutions', cls='sub_cat not_sortable')
 
-                        with ul(id='result_list'):
-                            self.result_list(imdbid)
-                        div(id='results_thinker')
+                            for res in resolutions:
+                                prior = '{}priority'.format(res)
+                                with li(cls='rbord', id=prior, sort=quality_settings['Quality'][res][1]):
+                                    i(cls='fa fa-bars')
+                                    i(id=res, cls='fa fa-square-o checkbox', value=quality_settings['Quality'][res][0])
+                                    span(res)
 
-                        # Panel that swaps in with quality adjustments
-                        resolutions = ['4K', '1080P', '720P', 'SD']
-                        with ul(id='quality', cls='wide'):
-                            # Resolution Block
-                            with ul(id='resolution', cls='sortable'):
-                                span('Resolutions', cls='sub_cat not_sortable')
+                        # Size restriction block
+                        with ul(id='resolution_size'):
+                            with li('Size Restrictions (MB)', cls='sub_cat'):
 
                                 for res in resolutions:
-                                    prior = '{}priority'.format(res)
-                                    with li(cls='rbord', id=prior, sort=quality_settings['Quality'][res][1]):
-                                        i(cls='fa fa-bars')
-                                        i(id=res, cls='fa fa-square-o checkbox', value=quality_settings['Quality'][res][0])
+                                    min = '{}min'.format(res)
+                                    max = '{}max'.format(res)
+                                    with li():
                                         span(res)
+                                        input(type='number', id=min, value=quality_settings['Quality'][res][2], min='0', style='width: 7.5em')
+                                        input(type='number', id=max, value=quality_settings['Quality'][res][3], min='0', style='width: 7.5em')
 
-                            # Size restriction block
-                            with ul(id='resolution_size'):
-                                with li('Size Restrictions (MB)', cls='sub_cat'):
+                        with ul(id='filters', cls='wide'):
+                            with li(cls='bbord flexbox'):
+                                span('Required words:')
+                                input(type='text', id='requiredwords', value=quality_settings['Filters']['requiredwords'], style='width: 16em')
+                            with li(cls='bbord flexbox'):
+                                span('Preferred words:')
+                                input(type='text', id='preferredwords', value=quality_settings['Filters']['preferredwords'], style='width: 16em')
+                            with li(cls='flexbox'):
+                                span('Ignored words:')
+                                input(type='text', id='ignoredwords', value=quality_settings['Filters']['ignoredwords'], style='width: 16em')
 
-                                    for res in resolutions:
-                                        min = '{}min'.format(res)
-                                        max = '{}max'.format(res)
-                                        with li():
-                                            span(res)
-                                            input(type='number', id=min, value=quality_settings['Quality'][res][2], min='0', style='width: 7.5em')
-                                            input(type='number', id=max, value=quality_settings['Quality'][res][3], min='0', style='width: 7.5em')
+            with div(id='plot'):
+                p(data['plot'])
+            with div(id='additional_info'):
+                with a(href=tomatoes_url, target='_blank'):
+                    span('Rotten Tomatoes Rating: {}'.format(data['tomatorating']))
+                span('Theatrical Release Date: {}'.format(data['released']))
+                span('DVD Release Date: {}'.format(data['dvd']))
 
-                            with ul(id='filters', cls='wide'):
-                                with li(cls='bbord flexbox'):
-                                    span('Required words:')
-                                    input(type='text', id='requiredwords', value=quality_settings['Filters']['requiredwords'], style='width: 16em')
-                                with li(cls='bbord flexbox'):
-                                    span('Preferred words:')
-                                    input(type='text', id='preferredwords', value=quality_settings['Filters']['preferredwords'], style='width: 16em')
-                                with li(cls='flexbox'):
-                                    span('Ignored words:')
-                                    input(type='text', id='ignoredwords', value=quality_settings['Filters']['ignoredwords'], style='width: 16em')
-
-                with div(id='plot'):
-                    p(data['plot'])
-                with div(id='additional_info'):
-                    with a(href=tomatoes_url, target='_blank'):
-                        span('Rotten Tomatoes Rating: {}'.format(data['tomatorating']))
-                    span('Theatrical Release Date: {}'.format(data['released']))
-                    span('DVD Release Date: {}'.format(data['dvd']))
-
-        return doc.render()
+        return unicode(container)
 
     def result_list(self, imdbid):
         results = self.sql.get_search_results(imdbid)

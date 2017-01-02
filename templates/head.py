f8d0e06da71ca7f72c70ad592197f45385e28310
soundjester <soundjester@users.noreply.github.com>
2017-01-02 07:59:39 -0600

    Add CSS Theming
    Updated location of sweetheart alert CSS to standard CSS location

--- templates/head.py
+++ templates/head.py
@@ -1,5 +1,6 @@
 from dominate.tags import *
 import core
+from core import config
 
 class Head(object):
 
@@ -10,11 +11,12 @@ class Head(object):
     def insert():
         meta(name='robots', content='noindex, nofollow')
         meta(name='url_base', content=core.URL_BASE)
-
-        link(rel='stylesheet', href=core.URL_BASE + '/static/css/style.css')
+        theme=str(core.CONFIG['Server']['csstheme'])
+        
+        link(rel='stylesheet', href=core.URL_BASE + '/static/css/'+theme+'/style.css')
         link(rel='stylesheet', href='//fonts.googleapis.com/css?family=Raleway')
         link(rel='stylesheet', href=core.URL_BASE + '/static/font-awesome/css/font-awesome.css')
-        link(rel='stylesheet', href=core.URL_BASE + '/static/js/sweetalert-master/dist/sweetalert.css')
+        link(rel='stylesheet', href=core.URL_BASE + '/static/css/'+theme+'/sweetalert.css')
         script(type='text/javascript', src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js')
         script(type='text/javascript', src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js')
         script(type='text/javascript', src=core.URL_BASE + '/static/js/sweetalert-master/dist/sweetalert-dev.js')

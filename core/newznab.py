nosmokingbandit/watcher

commit 481801d8b6b3c2ccf583319a12bf7e62e0ea3d16

Fix permalink info links

core/newznab.py
@@ -95,10 +95,7 @@ class NewzNab():
         for ic in item:
             if ic.tag in item_keep:
                 if ic.tag == 'guid' and ic.attrib['isPermaLink'] == 'false':
-                    print ic.tag
                     permalink = False
-                else:
-                    permalink = True
                 d[ic.tag.lower()] = ic.text
             if 'newznab' in ic.tag and ic.attrib['name'] == 'size':
                 d['size'] = int(ic.attrib['value'])
@@ -112,6 +109,7 @@ class NewzNab():
             d['info_link'] = d['comments']
         else:
             d['info_link'] = d['guid']
+
         del d['comments']
         d['guid'] = d['link']
         del d['link']

commit 43cfb1969ee599d7ad6994a84110aae602a41dbb

Initialize permalink with True
Parsing broke with commit 481801d.

core/newznab.py
@@ -92,6 +92,7 @@ class NewzNab():
 
         item_keep = ('title', 'category', 'link', 'guid', 'size', 'pubDate', 'comments')
         d = {}
+        permalink = True
         for ic in item:
             if ic.tag in item_keep:
                 if ic.tag == 'guid' and ic.attrib['isPermaLink'] == 'false':
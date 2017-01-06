acddfa52d02054ba27491a20697a8f87ed89b754
Florian Ehmke <imba3r@gmail.com>
2017-01-06 17:06:20 +0100

    Issue #102: Added option to disable fuzzy title matching.

--- core/scoreresults.py
+++ core/scoreresults.py
@@ -47,6 +47,7 @@ class ScoreResults():
             filters = core.CONFIG['Filters']
 
         retention = int(core.CONFIG['Search']['retention'])
+        score_title = bool(core.CONFIG['Search']['score_title'])
         required = filters['requiredwords'].lower().split(',')
         preferred = filters['preferredwords'].lower().split(',')
         ignored = filters['ignoredwords'].lower().split(',')
@@ -58,7 +59,8 @@ class ScoreResults():
         self.keep_required(required)
         self.retention_check(retention, today)
         self.score_quality(qualities)
-        self.fuzzy_title(title)
+        if score_title:
+            self.fuzzy_title(title)
         self.score_preferred(preferred)
 
         return self.results

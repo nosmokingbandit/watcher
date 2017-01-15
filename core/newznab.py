nosmokingbandit/watcher

commit 481801d8b6b3c2ccf583319a12bf7e62e0ea3d16

Fix permalink info links

core/newznab.py
@@ -95,10 +95,7 @@ class NewzNab():
         for ic in item:
             if ic.tag in item_keep:
                 if ic.tag == 'guid' and ic.attrib['isPermaLink'] == 'false':
-                    print ic.tag
                     permalink = False
-                else:
-                    permalink = True
                 d[ic.tag.lower()] = ic.text
             if 'newznab' in ic.tag and ic.attrib['name'] == 'size':
                 d['size'] = int(ic.attrib['value'])
@@ -112,6 +109,7 @@ class NewzNab():
             d['info_link'] = d['comments']
         else:
             d['info_link'] = d['guid']
+
         del d['comments']
         d['guid'] = d['link']
         del d['link']

commit a75b9f511db27f8e29766a91f3a5e3e7035f168e

Add 'log viewer' to 'settings' page (#147)
* Update .gitignore

* Initialize permalink with True (#100)

Parsing broke with commit 481801d.

* Update README.md

* Delete imdb

* Delete newpostscript.py

* Add 'log viewer' to 'settings'

* add 'log viewer' to 'settings' page

* CSS for 'log viewer' for 'settings' page

core/newznab.py
@@ -97,6 +97,7 @@ class NewzNab():
 
         item_keep = ('title', 'category', 'link', 'guid', 'size', 'pubDate', 'comments')
         d = {}
+        permalink = True
         for ic in item:
             if ic.tag in item_keep:
                 if ic.tag == u'guid' and ic.attrib['isPermaLink'] == u'false':

static/css/settings.css
@@ -246,3 +246,15 @@ div#footer{
     bottom: 1em;
     left: 1em;
 }
+
+/*For log file viewing*/
+pre {
+    padding: 2px;
+    border: 1px solid #666;
+    overflow: scroll;
+    max-height: 600px;
+    width: 135%;
+    max-width: 1000px;
+    font-size: .75em;
+    font-family: monospace;
+}

templates/header.py
@@ -47,6 +47,10 @@ class Header():
                                     with li():
                                         i(cls='fa fa-info-circle')
                                         span(u'About')
+                                with a(href=core.URL_BASE + '/settings/logs/'):
+                                    with li():
+                                        i(cls='fa fa-file-text')
+                                        span(u'Logs')
 
                     with a(href=core.URL_BASE + '/add_movie/'):
                         if current == 'add_movie':

templates/settings.py
@@ -429,6 +429,17 @@ class Settings():
                 with li():
                     a(u'Parse Torrent Name', href='https://pypi.python.org/pypi/parse-torrent-name')
 
+    @expose
+    @settings_page
+    def logs(self, c):
+        with div(cls='logs'):
+            h1(u'Log File')
+            with div(id='log-display'):
+                logfile = os.path.join(core.PROG_PATH, core.LOG_DIR, 'log.txt')
+                with open(logfile, 'r') as f:
+                    page = '%s' % f.read()
+                    pre(page)                    
+                    
     def get_themes(self):
         theme_path = os.path.join(core.PROG_PATH, 'static', 'css')
         themes = []
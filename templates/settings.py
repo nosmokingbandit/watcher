79f7ff7164ab82fbed500573407537d4b437d216
nosmokingbandit <nosmokingbandit@gmail.com>
2017-01-22 10:56:41 -0500

    Update version strings

--- templates/settings.py
+++ templates/settings.py
@@ -24,8 +24,8 @@ def settings_page(page):
         with doc.head:
             meta(name='git_url', content=core.GIT_URL)
             Head.insert()
-            link(rel='stylesheet', href=core.URL_BASE + '/static/css/settings.css?v=01.20')
-            link(rel='stylesheet', href=core.URL_BASE + '/static/css/{}/settings.css?v=01.20'.format(core.CONFIG['Server']['theme']))
+            link(rel='stylesheet', href=core.URL_BASE + '/static/css/settings.css?v=01.22')
+            link(rel='stylesheet', href=core.URL_BASE + '/static/css/{}/settings.css?v=01.22'.format(core.CONFIG['Server']['theme']))
             script(type='text/javascript', src=core.URL_BASE + '/static/js/settings/main.js?v=01.20b')
             script(type='text/javascript', src=core.URL_BASE + '/static/js/settings/save_settings.js?v=01.20')
 
